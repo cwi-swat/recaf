b388305b341c0766198914934a24de2e493ff2f7
Aggelos Biboudis <biboudis@gmail.com>
2016-01-19 16:56:27 +0100

    Add first async example
    *) still need to make references to adjust automatically

--- recaf-runtime/src/test-src/TestAsync.java
+++ recaf-runtime/src/test-src/TestAsync.java
@@ -1,23 +1,25 @@
-import draft.BuilderAlg;
-import higher.Async;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static java.util.Arrays.asList;
+
+import recaf.async.AsyncExtension;
 
 public class TestAsync {
-	
-	/*
-	void delayed(BuilderAlg<Async.t> alg) {
-		Async<Integer> answer = async {
-			System.out.println("Welcome...");
-			return 42;
-		}
-	}
-	translates to --->
-	*/
-	void desugared_delayed() {
-		BuilderAlg<Async.t> builder = new AsyncBuilder();
 
-		Async<Integer> answer = Async.prj(builder.Run(builder.Delay(() -> {
-			System.out.println("Welcome...");
-			return builder.Return(42);
-		})));
+	Future<Integer> async1(AsyncExtension<Integer> alg) {
+		AsyncExtension<Integer> $alg = alg;
+		return (Future<Integer>) $alg.Method($alg.Seq(asList($alg.If($alg.Exp(() -> {
+			return 1 > 5;
+		}), $alg.Return($alg.Exp(() -> {
+			return 42;
+		}))), $alg.Return($alg.Exp(() -> {
+			return 41;
+		})))));
+	}
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		Integer f2 = new TestAsync().async1(new AsyncExtension<Integer>()).get();
+		System.out.println(f2);
 	}
 }
--- recaf-runtime/src/test-src/TestIt.java
+++ recaf-runtime/src/test-src/TestIt.java
@@ -1,22 +1,33 @@
 
 import recaf.maybe.MaybeExtension;
 import java.util.Optional;
- 
+
 public class TestIt {
-  
-  Optional<Integer> maybe(MaybeExtension<Integer> alg) {
-  MaybeExtension<Integer> $alg = alg;   
-  return (Optional<Integer>)$alg.Method($alg.If($alg.Exp(() -> { return 1 > 5; }), $alg.Return($alg.Exp(() -> { return 42; }))));
-}
-  
-  Optional<Integer> readOptional(MaybeExtension<Integer> alg) {
-  MaybeExtension<Integer> $alg = alg;   
-  return (Optional<Integer>)$alg.Method($alg.Maybe($alg.Exp(() -> { return maybe(alg); }), (Integer x) -> {return $alg.Return($alg.Exp(() -> { return x + 1; }));}));
-}
-  public static void main(String args[]) {
-     Object x;
-     x  = new TestIt().readOptional(new MaybeExtension<Integer>());
-     System.out.println(x);
-  }
-  
+
+	Optional<Integer> maybe(MaybeExtension<Integer> alg) {
+		MaybeExtension<Integer> $alg = alg;
+		return (Optional<Integer>) $alg.Method($alg.If($alg.Exp(() -> {
+			return 1 > 5;
+		}), $alg.Return($alg.Exp(() -> {
+			return 42;
+		}))));
+	}
+
+	Optional<Integer> readOptional(MaybeExtension<Integer> alg) {
+		MaybeExtension<Integer> $alg = alg;
+		return (Optional<Integer>) $alg.Method($alg.Maybe($alg.Exp(() -> {
+			return maybe(alg);
+		}), (Integer x) -> {
+			return $alg.Return($alg.Exp(() -> {
+				return x + 1;
+			}));
+		}));
+	}
+
+	public static void main(String args[]) {
+		Object x;
+		x = new TestIt().readOptional(new MaybeExtension<Integer>());
+		System.out.println(x);
+	}
+
 }
