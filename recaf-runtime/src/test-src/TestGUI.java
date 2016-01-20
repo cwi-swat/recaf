0cb34a7b577aa8c9a99c70910c198ecaa19f839e
Tijs van der Storm <storm@cwi.nl>
2016-01-20 14:08:41 +0100

    GUI example working.

---
+++ recaf-runtime/src/test-src/TestGUI.java
@@ -0,0 +1,63 @@
+
+import recaf.gui.GUIExtension;
+import recaf.gui.RenderGUI;
+import recaf.gui.HandleGUI;
+import java.util.stream.IntStream;
+import java.util.Iterator;
+
+public class TestGUI {
+
+	static Void println(Object o) {
+		System.out.println(o);
+		return null;
+	}
+
+	Void h2(GUIExtension alg, String title) {
+		GUIExtension $alg = alg;
+		return (Void) $alg.Method($alg.Tag($alg.Exp(() -> {
+			return "h2";
+		}), $alg.Echo($alg.Exp(() -> {
+			return title;
+		}))));
+	}
+
+	Iterable<Integer> range(int n) {
+		return new Iterable<Integer>() {
+			public Iterator<Integer> iterator() {
+				return IntStream.range(0, n).iterator();
+			}
+		};
+	}
+
+	Void theGUI(GUIExtension alg, int n) {
+		GUIExtension $alg = alg;
+		return (Void) $alg.Method($alg.Seq($alg.ExpStat($alg.Exp(() -> {
+			return h2(alg, "Demo");
+		})), $alg.Tag($alg.Exp(() -> {
+			return "div";
+		}), $alg.For($alg.Exp(() -> {
+			return range(n);
+		}), (Integer i) -> {
+			return $alg.Tag($alg.Exp(() -> {
+				return "p";
+			}), $alg.Seq($alg.Echo($alg.Exp(() -> {
+				return "Hello " + i;
+			})), $alg.Button($alg.Exp(() -> {
+				return "Click " + i;
+			}), $alg.ExpStat($alg.Exp(() -> {
+				return println("clicked button " + i);
+			})))));
+		}))));
+	}
+
+	public static void main(String args[]) {
+		RenderGUI alg = new RenderGUI();
+		TestGUI tg = new TestGUI();
+		tg.theGUI(alg, 10);
+		String html = alg.getHTML();
+		System.out.println(html);
+		HandleGUI handle = new HandleGUI("id2");
+		tg.theGUI(handle, 10);
+	}
+
+}
