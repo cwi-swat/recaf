8943c5ca767166defa1be6e8f28bd3bb880f1eac
Tijs van der Storm <storm@cwi.nl>
2016-01-21 14:29:25 +0100

    Migrating to new annos.

--- recaf-runtime/src/main/java/recaf/maybe/Maybe.java
+++ recaf-runtime/src/main/java/recaf/maybe/Maybe.java
@@ -1,6 +1,33 @@
 package recaf.maybe;
 
-public @interface Maybe {
-	@SuppressWarnings("rawtypes")
-	public static final Class<MaybeExtension> builder = MaybeExtension.class;
+import java.util.Optional;
+import java.util.function.Function;
+
+import recaf.core.AbstractJavaCPS;
+import recaf.core.ED;
+import recaf.core.Ref;
+import recaf.core.SD;
+
+
+public class Maybe<R> extends AbstractJavaCPS<R> {
+
+	public Optional<R> Method(SD<R> body) {
+		Ref<Optional<R>> ref = new Ref<Optional<R>>();
+		body.accept(r -> { ref.x = Optional.of(r); }, 
+				() -> {  ref.x = Optional.empty(); }, 
+				exc -> { throw new RuntimeException(exc); });
+		return ref.x;
+	}
+	
+	public <U> SD<R> Maybe(ED<Optional<U>> opt, Function<U, SD<R>> body) {
+		return (rho, sigma, err) -> opt.accept(v -> {
+			if (v.isPresent()) {
+				body.apply(v.get()).accept(rho, sigma, err);
+			}
+			else {
+				sigma.call();
+			}
+		}, err);
+	}
+	
 }
--- recaf-runtime/src/test-src/TestMaybe.java
+++ recaf-runtime/src/test-src/TestMaybe.java
@@ -1,36 +1,46 @@
 
-import recaf.maybe.MaybeExtension;
+import recaf.maybe.Maybe;
 import java.util.Optional;
- 
+
 public class TestMaybe {
 
-  static Void println(Object o) {
-    System.out.println(o);
-    return null;
-  }
-  
-  Optional<Integer> maybe(MaybeExtension<Integer> alg) {
-  return (Optional<Integer>)alg.Method(alg.<Integer>Decl(alg.Exp(() -> { return 3; }), x -> {return alg.Seq(alg.ExpStat(alg.Exp(() -> { return println("maybe"); })), alg.If(alg.Exp(() -> { return 7 > 5; }), alg.Seq(alg.ExpStat(alg.Exp(() -> { return println("Yes"); })), alg.Return(alg.Exp(() -> { return 42; })))));}));
-}
-  
-  Optional<Integer> readOptional() {
-    return readOptional(new MaybeExtension<Integer>());
-  }
-  
-  
-  Optional<Integer> readOptional(MaybeExtension<Integer> alg) {
-  return (Optional<Integer>)alg.Method(alg.Maybe(alg.Exp(() -> { return maybe(alg); }), (Integer x) -> {return alg.Return(alg.Exp(() -> { return x + 1; }));}));
-}
-  
-  void doSomething() {
-    int x = 10;
-    
-  } 
-  
-  public static void main(String args[]) {
-     Object x;
-     x  = new TestMaybe().readOptional(new MaybeExtension<Integer>());
-     System.out.println(x);
-  }
-  
+	static Void println(Object o) {
+		System.out.println(o);
+		return null;
+	}
+
+	Optional<Integer> maybe() {
+		Maybe<Integer> $alg = new Maybe<Integer>();
+		return (Optional<Integer>) $alg.Method($alg.<Integer> Decl($alg.Exp(() -> {
+			return 3;
+		}), x -> {
+			return $alg.Seq($alg.ExpStat($alg.Exp(() -> {
+				return println("maybe");
+			})), $alg.If($alg.Exp(() -> {
+				return 7 > 5;
+			}), $alg.Seq($alg.ExpStat($alg.Exp(() -> {
+				return println("Yes");
+			})), $alg.Return($alg.Exp(() -> {
+				return 42;
+			})))));
+		}));
+	}
+
+	Optional<Integer> readOptional() {
+		Maybe<Integer> $alg = new Maybe<Integer>();
+		return (Optional<Integer>) $alg.Method($alg.Maybe($alg.Exp(() -> {
+			return maybe();
+		}), (Integer x) -> {
+			return $alg.Return($alg.Exp(() -> {
+				return x + 1;
+			}));
+		}));
+	}
+
+	public static void main(String args[]) {
+		Object x;
+		x = new TestMaybe().readOptional();
+		System.out.println(x);
+	}
+
 }
