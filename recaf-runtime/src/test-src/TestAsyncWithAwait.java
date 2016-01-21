97b099a2ef5b5575b51694581d4f46e573d2802e
Aggelos Biboudis <biboudis@gmail.com>
2016-01-21 15:18:37 +0100

    Merge with map

--- recaf-runtime/src/test-src/TestAsyncWithAwait.java
+++ recaf-runtime/src/test-src/TestAsyncWithAwait.java
@@ -7,13 +7,11 @@ import recaf.async.AsyncExtension;
 public class TestAsyncWithAwait {
 
 	CompletableFuture<Integer> op(AsyncExtension<Integer> alg) {
-  AsyncExtension<Integer> $alg = alg;   
-  return (CompletableFuture<Integer>)$alg.Method($alg.Seq($alg.If($alg.Exp(() -> { return 1 < 2; }), $alg.Await($alg.Exp(() -> { return secondOp(alg); }), (Integer x) -> {return $alg.Return($alg.Exp(() -> { return x; }));})), $alg.Return($alg.Exp(() -> { return 41; }))));
+  return (CompletableFuture<Integer>)alg.Method(alg.Seq(alg.If(alg.Exp(() -> { return 1 < 2; }), alg.Await(alg.Exp(() -> { return secondOp(alg); }), (Integer x) -> {return alg.Return(alg.Exp(() -> { return x; }));})), alg.Return(alg.Exp(() -> { return 41; }))));
 } 
   	
   	CompletableFuture<Integer> secondOp(AsyncExtension<Integer> alg) {
-  AsyncExtension<Integer> $alg = alg;   
-  return (CompletableFuture<Integer>)$alg.Method($alg.Seq($alg.If($alg.Exp(() -> { return 1 < 2; }), $alg.Return($alg.Exp(() -> { return 42; }))), $alg.Return($alg.Exp(() -> { return 41; }))));
+  return (CompletableFuture<Integer>)alg.Method(alg.Seq(alg.If(alg.Exp(() -> { return 1 < 2; }), alg.Return(alg.Exp(() -> { return 42; }))), alg.Return(alg.Exp(() -> { return 41; }))));
 }
   	
   	public static void main(String[] args) throws InterruptedException, ExecutionException{
