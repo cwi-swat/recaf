fb00d1f44af20351668479ab0aabe63c7256c2be
Tijs van der Storm <storm@cwi.nl>
2016-01-22 22:29:37 +0100

    Another kind of algebra.

---
+++ recaf-runtime/src/main/java/recaf/core/JavaStmtAlg.java
@@ -0,0 +1,45 @@
+package recaf.core;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public interface JavaStmtAlg<S> {
+	S Exp(Supplier<?> e);
+	
+	<T> S Decl(Supplier<T> exp, Function<T, S> body);
+
+	<T extends Iterable<?>> S For(Supplier<T> coll, Function<T, S> body);
+	
+	S If(Supplier<Boolean> e, S s);
+	
+	S If(Supplier<Boolean> e, S s1, S s2);
+	
+	S While(Supplier<Boolean> e, S s);
+	
+	S DoWhile(S s, Supplier<Boolean> e);
+	
+	S Labeled(String label, S s);
+	
+	S Break(String label);
+	
+	S Continue(String label);
+	
+	S Break();
+	
+	S Continue();
+	
+	S Return(Supplier<?> e);
+	
+	S Return();
+
+	S Throw(Supplier<? extends Throwable> e);
+
+	S Empty();
+	
+	S Seq(S s1, S s2);
+	
+	<T extends Throwable> S TryCatch(S body, Class<T> type, Function<T, S> handle);
+
+	S TryFinally(S body, S fin);
+	
+}
