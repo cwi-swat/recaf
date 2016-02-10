b455f8ac8e3272e7797f817d2406f257c3464d86
Tijs van der Storm <storm@cwi.nl>
2016-02-10 14:04:48 +0100

    Apparently, the nillary constructor was used...

--- recaf-runtime/src/main/java/recaf/core/Ref.java
+++ recaf-runtime/src/main/java/recaf/core/Ref.java
@@ -3,7 +3,12 @@ package recaf.core;
 // todo: extract interface to make useable with fields in expression eval.
 public final class Ref<X> {
 	public X value ;
-	public Ref(X value){
+	public Ref(X value) {
 		this.value = value;
 	}
+
+	public Ref() {
+		this(null);
+	}
+
 }
