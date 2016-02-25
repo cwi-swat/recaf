5c4d148f7a6c2e4161c5163985c8cdf912d6b479
Aggelos Biboudis <biboudis@gmail.com>
2016-02-25 15:21:20 +0100

    Experimenting with stream

--- recaf-runtime/src/main/java/recaf/demo/cps/StreamExt.java
+++ recaf-runtime/src/main/java/recaf/demo/cps/StreamExt.java
@@ -1,5 +1,6 @@
 package recaf.demo.cps;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -7,28 +8,51 @@ import recaf.core.alg.JavaMethodAlg;
 import recaf.core.cps.SD;
 import recaf.core.cps.StmtJava;
 import recaf.demo.cps.stream.Stream;
+import rx.Observable;
+import rx.Subscriber;
 
-public class StreamExt<R> implements StmtJava<R>, JavaMethodAlg<Stream<R>, SD<R>> {
+public class StreamExt<R> implements StmtJava<R>, JavaMethodAlg<Observable<R>, SD<R>> {
 	
-	public Stream<R> Method(SD<R> body) {
-		throw new UnsupportedOperationException();
+	Observable<R> result;
+	public Observable<R> Method(SD<R> body) {
+		
+		result = Observable.create(subscriber -> {
+			body.accept(null,
+					r ->     { }, 
+					() ->    { },
+					label -> { },
+					label -> { },
+					ex ->    { subscriber.onError(ex); });
+		});
+		
+		return result;
 	}
 	
-	public <T> SD<R> Await(Supplier<Stream<T>> e, Function<T, SD<R>> body) {
+	public <T> SD<R> Await(Supplier<CompletableFuture<T>> e, Function<T, SD<R>> body) {
 		return (label, rho, sigma, brk, contin, err) -> get(e).accept(f -> {
-			throw new UnsupportedOperationException();
+			f.whenComplete((a, ex) -> {
+				if (a == null) {
+					err.accept(ex);
+				} else {
+					body.apply(a).accept(null, rho, sigma, brk, contin, err);
+				}
+			});
 		} , err);
 	}
 	
 	public <U> SD<R> Yield(Supplier<U> exp) {
 		return (label, rho, sigma, brk, contin, err) -> {
 			get(exp).accept(v -> {
-				throw new UnsupportedOperationException();
+				// result.doOnNext(v);
 			} , err);
 		};
 	}
 
-	public <U> SD<R> YieldFrom(Supplier<Iterable<U>> exp) {
-		return ForEach(exp, e -> Yield(() -> e));
+	public <U> SD<R> YieldFrom(Supplier<Observable<U>> exp) {
+		return (label, rho, sigma, brk, contin, err) -> {
+			get(exp).accept(v -> {
+				// result.mergeWith(v);
+			} , err);
+		};
 	}
 }
