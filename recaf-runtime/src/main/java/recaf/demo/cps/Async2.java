e896e1c036ac1835923b4d219395e310b6b5f16d
pinoval <pvaldera@cwi.nl>
2016-03-01 20:38:05 +0100

    Adding new Async example based on expressions
    Signed-off-by: pinoval <pvaldera@cwi.nl>

---
+++ recaf-runtime/src/main/java/recaf/demo/cps/Async2.java
@@ -0,0 +1,42 @@
+package recaf.demo.cps;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import recaf.core.IRef;
+import recaf.core.alg.JavaMethodAlg;
+import recaf.core.cps.FullJava;
+import recaf.core.cps.SD;
+
+public class Async2<R> implements FullJava<R>, JavaMethodAlg<Future<R>, SD<R>> {
+
+	public Future<R> Method(SD<R> body) {
+		CompletableFuture<R> promise = new CompletableFuture<R>();
+
+		CompletableFuture.supplyAsync(() -> {
+			body.accept(null,
+					r -> promise.complete(r), 
+					() -> promise.complete(null),
+					l -> {throw new AssertionError("cannot break without loop");},
+					l -> {throw new AssertionError("cannot break without loop");},
+					ex -> promise.completeExceptionally(ex));
+			return null;
+		});
+
+		return promise;
+	}
+
+	public <T> SD<R> Await(Supplier<CompletableFuture<IRef<T>>> e, Function<IRef<T>, SD<R>> body) {
+		return (label, rho, sigma, brk, contin, err) -> get(e).accept(f -> {
+			f.whenComplete((a, ex) -> {
+				if (a == null) {
+					err.accept(ex);
+				} else {
+					body.apply(a).accept(null, rho, sigma, brk, contin, err);
+				}
+			});
+		} , err);
+	}
+}
