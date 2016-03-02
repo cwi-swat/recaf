340cab5de2dc32ed4bb7473f9a7f67c338d71a66
pinoval <pvaldera@cwi.nl>
2016-03-02 17:00:23 +0100

    Fixing method and constructor lookup (dull visibility assumption)
    Signed-off-by: pinoval <pvaldera@cwi.nl>

--- recaf-runtime/src/main/java/recaf/core/EvalJavaHelper.java
+++ recaf-runtime/src/main/java/recaf/core/EvalJavaHelper.java
@@ -1,6 +1,8 @@
 package recaf.core;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+
 import org.apache.commons.beanutils.MethodUtils;
 
 public class EvalJavaHelper {
@@ -15,13 +17,18 @@ public class EvalJavaHelper {
 	// TODO implement proper overloading methods resolution
 	public static Method findMethod(Object o, String methodName, Object[] args){
 		Class<?> clazz = o.getClass();
-		for (Method m : clazz.getDeclaredMethods()){
-			if (m.getName() == methodName){
-				if (m.getParameterCount() == args.length){
-					if (areAssignable(m.getParameterTypes(), args))
-						return m;
+		while (clazz != null) {
+		    Method[] methods = clazz.getDeclaredMethods();
+		    for (Method m : methods){
+				if (m.getName() == methodName){
+					if (m.getParameterCount() == args.length){
+						if (areAssignable(m.getParameterTypes(), args))
+							return m;
+					}
 				}
 			}
+			
+		    clazz = clazz.getSuperclass();
 		}
 		return null;
 	}
@@ -37,6 +44,20 @@ public class EvalJavaHelper {
 
 	private static boolean isAssignable(Class<?> methodParameter, Object o) {
 		return MethodUtils.isAssignmentCompatible(methodParameter, o.getClass());
+	}
+
+	public static Constructor<?> findConstructor(Class<?> clazz, Object[] args) {
+		while (clazz != null) {
+		    Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+		    for (Constructor<?> c : constructors){
+				if (c.getParameterCount() == args.length){
+					if (areAssignable(c.getParameterTypes(), args))
+							return c;
+				}
+			}
+		    clazz = clazz.getSuperclass();
+		}
+		return null;
 	}
 	
 }
