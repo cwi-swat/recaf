828c3c5ab6a8a55e1a36e661febf8a342490107a
pinoval <pvaldera@cwi.nl>
2016-03-03 19:41:42 +0100

    More tests on expressions
    Signed-off-by: pinoval <pvaldera@cwi.nl>

--- recaf-runtime/src/test/java/recaf/tests/virtualization/TestVirtualizationExpressions.java
+++ recaf-runtime/src/test/java/recaf/tests/virtualization/TestVirtualizationExpressions.java
@@ -9,24 +9,36 @@ import recaf.tests.CompiletimeException;
 public class TestVirtualizationExpressions extends BaseTest {
 	
 	@Test
-	public void TestMethodLookup1() throws CompiletimeException, RuntimeException {
-		String output = compileAndRun("TestExprMethodLookup1");
+	public void TestPrivateConstructor() throws CompiletimeException, RuntimeException {
+		String output = compileAndRun("TestExprPrivateConstructor");
 
+		Assert.assertEquals("AA(0)", output);
+	}
+	
+	@Test
+	public void TestNoConstructor() throws CompiletimeException, RuntimeException {
+		String output = compileAndRun("TestExprNoConstructor");
+
+		Assert.assertEquals("AB(0)", output);
+	}
+	
+	@Test
+	public void TestSimpleMethodLookup() throws CompiletimeException, RuntimeException {
+		String output = compileAndRun("TestExprSimpleMethodLookup");
 		Assert.assertEquals("4", output);
 	}
 	
 	@Test
-	public void TestMethodLookup2() throws CompiletimeException, RuntimeException {
-		String output = compileAndRun("TestExprMethodLookup2");
-
-		Assert.assertEquals("4", output);
+	public void TestInheritanceMethodLookup() throws CompiletimeException, RuntimeException {
+		String output = compileAndRun("TestExprInheritanceMethodLookup");
+		Assert.assertEquals("-1", output);
 	}
 	
-	@Test
-	public void TestMethodLookup3() throws CompiletimeException, RuntimeException {
-		String output = compileAndRun("TestExprMethodLookup3");
 
-		Assert.assertEquals("4", output);
+	@Test
+	public void TestPlus() throws CompiletimeException, RuntimeException {
+		String output = compileAndRun("TestExprPlus");
+		Assert.assertEquals("2\n2.0\n2.0\n2.0\n11\n11\n11.0\n1.01\n1.01\n11.0\n2.0\n2.0\n2.0\n2.0\n2.0\n11", output);
 	}
 
 }
