8bf78f02050fd9fee4efbe3bd51256f73908988f
Tijs van der Storm <storm@cwi.nl>
2016-03-08 23:18:01 +0100

    Paper stuff

---
+++ recaf-runtime/src/main/java/recaf/paper/MuJava.java
@@ -0,0 +1,17 @@
+package recaf.paper;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+//BEGIN_MUJAVA_ALG
+public interface MuJava<R, S> {
+	R Method(S s);
+	S Exp(Supplier<Void> e);
+	<T> S Decl(Supplier<T> e, Function<T, S> s);
+	<T> S For(Supplier<Iterable<T>> e, Function<T, S> s);
+	S If(Supplier<Boolean> c, S s1, S s2);
+	S Return(Supplier<R> e);
+	S Seq(S s1, S s2);
+	S Empty();
+}
+//END_MUJAVA_ALG
---
+++ recaf-runtime/src/main/java/recaf/paper/MuJavaBase.java
@@ -0,0 +1,51 @@
+package recaf.paper;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+interface IExec {
+	void exec();
+}
+
+class Return extends RuntimeException { 
+	Object value;
+
+	Return(Object value) {
+		this.value = value;
+	}
+}
+
+public class MuJavaBase<R> implements MuJava<R, IExec> {
+	public R Method(IExec s) {
+		try { s.exec(); } catch (Return r) { return (R)r.value; }
+		return null;
+	}
+
+	public IExec Exp(Supplier<Void> e) {
+		return () -> { e.get(); };
+	}
+
+	public <T> IExec Decl(Supplier<T> e, Function<T, IExec> s) {
+		return () -> s.apply(e.get()).exec();
+	}
+
+	public <T> IExec For(Supplier<Iterable<T>> e, Function<T, IExec> s) {
+		return () -> { for (T t: e.get()) s.apply(t).exec(); };
+	}
+
+	public IExec If(Supplier<Boolean> c, IExec s1, IExec s2) {
+		return () -> { if (c.get()) s1.exec(); else  s2.exec(); };
+	}
+
+	public IExec Return(Supplier<R> e) {
+		return () -> { throw new Return(e.get()); };
+	}
+
+	public IExec Seq(IExec s1, IExec s2) {
+		return () -> { s1.exec(); s2.exec(); };
+	}
+
+	public IExec Empty() {
+		return () -> {};
+	}
+}
