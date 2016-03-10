fce9b326f5b1e511ce23fc7bd085f6a8aba2e486
Aggelos Biboudis <biboudis@gmail.com>
2016-03-10 16:57:44 +0100

    Experimenting

--- recaf-runtime/src/main/java/recaf/demo/cps/StreamExt.java
+++ recaf-runtime/src/main/java/recaf/demo/cps/StreamExt.java
@@ -41,6 +41,8 @@ public class StreamExt<R> implements StmtJava<R>, JavaMethodAlg<Subject<R, R>, S
 	}
 	
 	public SD<R> Yield(ISupply<R> exp) {
+		System.out.println("Yield");
+
 		return (label, rho, sigma, brk, contin, err) -> {
 			get(exp).accept(v -> { 
 				result.onNext(v);
