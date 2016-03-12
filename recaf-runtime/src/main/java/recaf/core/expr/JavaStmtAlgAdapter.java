aa7fe6a700c0a30c97f50e240b36156535a3251a
Tijs van der Storm <storm@cwi.nl>
2016-03-12 18:14:31 +0100

    Adapter.

---
+++ recaf-runtime/src/main/java/recaf/core/expr/JavaStmtAlgAdapter.java
@@ -0,0 +1,138 @@
+package recaf.core.expr;
+
+import java.util.function.Function;
+
+import recaf.core.Ref;
+import recaf.core.alg.JavaStmtAlg;
+import recaf.core.direct.IEval;
+
+public interface JavaStmtAlgAdapter<R, S, C> extends StmtAlg<R, IEval, S, C>{
+	JavaStmtAlg<R, S, C> base();
+
+	@Override
+	default <T> S Decl(IEval exp, Function<Ref<T>, S> body) {
+		return base().Decl(() -> (T)exp.eval(), body);
+	}
+
+	@Override
+	default <T> S ForEach(IEval exp, Function<Ref<T>, S> body) {
+		return base().ForEach(() -> (Iterable)exp.eval(), body); 
+	}
+
+	@Override
+	default <T> S ForDecl(IEval init, Function<Ref<T>, S> body) {
+		return base().ForDecl(() -> (T)init.eval(), body); 
+	}
+
+	@Override
+	default S ForBody(IEval cond, S update, S body) {
+		return base().ForBody(() -> (Boolean)cond.eval(), update, body);
+	}
+
+	@Override
+	default <T extends Throwable> S Throw(IEval e) {
+		return base().Throw(() -> (Throwable)e.eval());
+	}
+
+	@Override
+	default <T extends Throwable> S TryCatch(S body, Class<T> type, Function<T, S> handle) {
+		return base().TryCatch(body, type, handle);
+	}
+
+	@Override
+	default <T> S Switch(IEval expr, C... cases) {
+		return base().Switch(() -> expr.eval(), cases);
+	}
+
+	@Override
+	default <T> C Case(T constant, S expStat) {
+		return base().Case(constant, expStat);
+	}
+
+	@Override
+	default C Default(S expStat) {
+		return base().Default(expStat);
+	}
+
+	@Override
+	default S For(S init, IEval cond, S update, S body) {
+		return base().For(init, () -> (Boolean)cond.eval(), update, body);
+	}
+
+	@Override
+	default S If(IEval cond, S s) {
+		return base().If(() -> (Boolean)cond.eval(), s);
+	}
+
+	@Override
+	default S If(IEval cond, S s1, S s2) {
+		return base().If(() -> (Boolean)cond.eval(), s1, s2);
+	}
+
+	@Override
+	default S While(IEval cond, S s) {
+		return base().While(() -> (Boolean)cond.eval(), s);
+	}
+
+	@Override
+	default S DoWhile(S s, IEval cond) {
+		return base().DoWhile(s, () -> (Boolean)cond.eval());
+	}
+
+	@Override
+	default S Labeled(String label, S s) {
+		return base().Labeled(label, s);
+	}
+
+	@Override
+	default S Break(String label) {
+		return base().Break(label);
+	}
+
+	@Override
+	default S Continue(String label) {
+		return base().Continue(label);
+	}
+
+	@Override
+	default S Break() {
+		return base().Break();
+	}
+
+	@Override
+	default S Continue() {
+		return base().Break();
+	}
+
+	@Override
+	default S Return(IEval supplier) {
+		return base().Return(() -> (R)supplier.eval());
+	}
+
+	@Override
+	default S Return() {
+		return base().Return();
+	}
+
+	@Override
+	default S Empty() {
+		return base().Empty();
+	}
+
+	@Override
+	default S Seq(S s1, S s2) {
+		return base().Seq(s1, s2);
+	}
+
+	@Override
+	default S TryFinally(S body, S fin) {
+		return base().TryFinally(body, fin);
+	}
+
+	@Override
+	default S ExpStat(IEval e) {
+		return base().ExpStat(() -> { e.eval(); return null; });
+	}
+	
+	
+}
---
+++ recaf-runtime/src/main/java/recaf/core/expr/StmtAlg.java
@@ -0,0 +1,58 @@
+package recaf.core.expr;
+
+import java.util.function.Function;
+
+import recaf.core.Ref;
+
+public interface StmtAlg<R, E, S, C> {
+	<T> S Decl(E exp, Function<Ref<T>, S> body);
+
+	<T> S ForEach(E exp, Function<Ref<T>, S> body);
+
+	<T> S ForDecl(E init, Function<Ref<T>, S> body);
+
+	S ForBody(E cond, S update, S body);
+
+	<T extends Throwable> S Throw(E e);
+
+	<T extends Throwable> S TryCatch(S body, Class<T> type, Function<T, S> handle);
+
+	@SuppressWarnings("unchecked")
+	<T> S Switch(E expr, C... cases);
+
+	<T> C Case(T constant, S expStat);
+
+	C Default(S expStat);
+
+	S For(S init, E cond, S update, S body);
+
+	S If(E cond, S s);
+
+	S If(E cond, S s1, S s2);
+
+	S While(E cond, S s);
+
+	S DoWhile(S s, E cond);
+
+	S Labeled(String label, S s);
+
+	S Break(String label);
+
+	S Continue(String label);
+
+	S Break();
+
+	S Continue();
+
+	S Return(E supplier);
+
+	S Return();
+
+	S Empty();
+
+	S Seq(S s1, S s2);
+
+	S TryFinally(S body, S fin);
+
+	S ExpStat(E e);
+}
