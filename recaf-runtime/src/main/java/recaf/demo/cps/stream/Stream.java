721c9145624d47bcbe066fcd05d0b8b8ab161bbe
Aggelos Biboudis <biboudis@gmail.com>
2016-03-14 17:43:44 +0100

    Add convenient scripts

--- recaf-runtime/src/main/java/recaf/demo/cps/stream/Stream.java
+++
@@ -1,9 +0,0 @@
-package recaf.demo.cps.stream;
-
-import java.util.function.Consumer;
-
-import recaf.core.cps.K0;
-
-public interface Stream<T> {
-	StreamSubscription<T> listen(Consumer<T> onData, Consumer<Exception> orError, K0 onDone);
-}
