d737625747327380ea60b8c47a3c9193463a4379
pinoval <pvaldera@cwi.nl>
2016-03-14 12:38:34 +0100

    Back to old memo example
    Signed-off-by: pinoval <pvaldera@cwi.nl>

--- recaf-runtime/src/main/java/recaf/demo/direct/Memo.java
+++ recaf-runtime/src/main/java/recaf/demo/direct/Memo.java
@@ -1,42 +1,31 @@
 package recaf.demo.direct;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import static recaf.core.expr.EvalJavaHelper.toValue;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 import recaf.core.direct.IEval;
-import recaf.core.full.FullJavaDirect;
-
-import static recaf.core.expr.EvalJavaHelper.toValue;
+import recaf.core.expr.EvalJavaHelper;
+import recaf.core.full.FullJavaDirect;;
 
 public class Memo<R> implements FullJavaDirect<R> {
 	
-	private Map<Object,Object> memo = new HashMap<>();
+	private Map<MemoizableCall,R> map = new HashMap<MemoizableCall,R>();
+	
+	public Memo(){
+	}
 	
 	@Override
-	public IEval Invoke(IEval x, String m, IEval... es) {
-		// NB: (I)Refs should hash/equals on their contained value.
+	public IEval Invoke(IEval obj, String method, IEval... args) {
 		return () -> {
-			List<Object> key = new ArrayList<>(es.length + 2);
-			key.add(toValue(x.eval()));
-			key.add(m);
-			for (int i = 0; i < es.length; i++) 
-				key.add(toValue(es[i].eval()));
-			if (!memo.containsKey(key)) {
-				IEval[] es2 = Arrays.copyOf(es, es.length);
-				IntStream.range(0, es.length).forEach(i -> {
-					es2[i] = () -> key.get(i + 2);	
-				});
-				Object val = FullJavaDirect.super.Invoke(() -> key.get(0), m, es2).eval();
-				memo.put(key, val);
-			}
-			else {
-				//System.out.println("Memoized call: " + key);
-			}
-			return memo.get(key);
+			Object evObj = toValue(obj.eval());
+			Object[] evArgs = EvalJavaHelper.evaluateArguments(args);
+			MemoizableCall key = new MemoizableCall(evObj, method, evArgs);
+			if (!map.containsKey(key))
+				map.put(key, (R) FullJavaDirect.super.Invoke(EvalJavaHelper.delayObject(evObj), method, EvalJavaHelper.delayObjects(evArgs)).eval());
+			return map.get(key);			
 		};
 	}
+
 }
