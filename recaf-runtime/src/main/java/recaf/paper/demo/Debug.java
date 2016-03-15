e4afb611e22e9119ecadec1d7b34a174ebe561e0
pinoval <pvaldera@cwi.nl>
2016-03-15 18:19:37 +0100

    Simplified debug example
    Signed-off-by: pinoval <pvaldera@cwi.nl>

--- recaf-runtime/src/main/java/recaf/paper/demo/Debug.java
+++ recaf-runtime/src/main/java/recaf/paper/demo/Debug.java
@@ -1,19 +1,15 @@
 package recaf.paper.demo;
 
-import java.util.logging.Logger;
-
 import recaf.paper.expr.IEval;
 import recaf.paper.expr.MuStmPrintEvalAdapter;
 import recaf.paper.expr.Pair;
-import recaf.paper.expr.PrintAndEval;
+import recaf.paper.expr.PrintAndEvalReusing;
 import recaf.paper.methods.TPDirect;
 import recaf.paper.stm.IExec;
 import recaf.paper.stm.MuJava;
 import recaf.paper.stm.MuJavaBase;
 
-public class Debug<R> implements MuStmPrintEvalAdapter<R, IExec>, PrintAndEval, TPDirect<R> {
-
-	static Logger log = Logger.getLogger(Debug.class.getName());
+public class Debug<R> implements MuStmPrintEvalAdapter<R, IExec>, PrintAndEvalReusing, TPDirect<R> {
 
 	@Override
 	public MuJava<R, IExec> base() {
@@ -29,8 +25,7 @@ public class Debug<R> implements MuStmPrintEvalAdapter<R, IExec>, PrintAndEval, 
 			} catch (Throwable t) {
 				throw new RuntimeException(t);
 			}
-			log.info(e.snd + "=> " + result);
+			System.out.println("DEBUG: "+e.snd + " => " + result);
 		};
 	}
-
 }
