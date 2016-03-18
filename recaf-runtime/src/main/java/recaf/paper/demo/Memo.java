059cdfeb52d7907d88825f72bb9815d971276658
pinoval <pvaldera@cwi.nl>
2016-03-18 14:27:29 +0100

    Changed memo
    Signed-off-by: pinoval <pvaldera@cwi.nl>

--- recaf-runtime/src/main/java/recaf/paper/demo/Memo.java
+++ recaf-runtime/src/main/java/recaf/paper/demo/Memo.java
@@ -1,7 +1,6 @@
 package recaf.paper.demo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,40 +10,39 @@ import recaf.paper.expr.IEval;
 import recaf.paper.expr.MuExpJavaBase;
 
 interface Memo extends MuExpJavaBase {
-	
-	static List<Object> eval(IEval... es) throws Throwable {
+
+	static List<Object> eval(IEval... es) {
 		List<Object> lst = new ArrayList<>(es.length);
-		for (int i = 0; i < es.length; i++) { 
+		for (int i = 0; i < es.length; i++) {
 			lst.add(es[i].eval());
 		}
 		return lst;
 	}
-	
+
 	static IEval[] uneval(List<Object> vs) {
 		IEval[] es = new IEval[vs.size()];
 		IntStream.range(0, vs.size()).forEach(i -> {
-			es[i] = () -> vs.get(i + 2);	
+			es[i] = () -> vs.get(i + 2);
 		});
 		return es;
 	}
-	
-	//BEGIN_MEMO
+
+	// BEGIN_MEMO
 	static Map<List<Object>, Object> memo = new HashMap<>();
-	
+
 	default IEval Invoke(IEval x, String m, IEval... es) {
 		return () -> {
 			List<Object> key = new ArrayList<>();
-			key.add(x.eval()); key.add(m);
+			key.add(x.eval());
+			key.add(m);
 			List<Object> vs = eval(es);
 			key.addAll(vs);
 			if (!memo.containsKey(key)) {
-				Object val = MuExpJavaBase.super
-				  .Invoke(() -> key.get(0), m, uneval(vs))
-				  .eval();
+				Object val = MuExpJavaBase.super.Invoke(() -> key.get(0), m, uneval(vs)).eval();
 				memo.put(key, val);
 			}
 			return memo.get(key);
 		};
 	}
-	//END_MEMO
+	// END_MEMO
 }
