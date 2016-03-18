fc2da094b58695cd8c2ad311cde70f0baa4870e9
Tijs van der Storm <storm@cwi.nl>
2016-03-18 13:54:44 +0100

    No more checked exception

--- recaf-runtime/src/main/java/recaf/paper/full/MuStmGenericJavaAdapter.java
+++ recaf-runtime/src/main/java/recaf/paper/full/MuStmGenericJavaAdapter.java
@@ -1,6 +1,7 @@
 package recaf.paper.full;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import recaf.paper.stm.MuJava;
 
@@ -8,31 +9,31 @@ import recaf.paper.stm.MuJava;
 public interface MuStmGenericJavaAdapter<R, S, E> extends MuStmJava<S, E> {
 	MuJava<R, S> base();
 	
-	<T> T adapt(E e);
+	<T> Supplier<T> adapt(E e);
 	
 	@Override
 	default S Exp(E e) {
-		return base().Exp(() -> { adapt(e); return null;});
+		return base().Exp(adapt(e));
 	}
 	
 	@Override
 	default <T> S Decl(E x, Function<T, S> s) {
-		return base().Decl(() -> adapt(x), s); 
+		return base().Decl(adapt(x), s); 
 	}
 	
 	@Override
 	default <T> S For(E e, Function<T, S> s) {
-		return base().For(() -> (Iterable<T>) adapt(e), s);
+		return base().For(adapt(e), s);
 	}
 	
 	@Override
 	default S If(E c, S s1, S s2) {
-		return base().If(() -> adapt(c), s1, s2);
+		return base().If(adapt(c), s1, s2);
 	}
 	
 	@Override
 	default S Return(E e) {
-		return base().Return(() -> adapt(e));
+		return base().Return(adapt(e));
 	}
 	
 	@Override
--- recaf-runtime/src/main/java/recaf/paper/full/MuStmIEvalJavaAdapter.java
+++ recaf-runtime/src/main/java/recaf/paper/full/MuStmIEvalJavaAdapter.java
@@ -1,13 +1,21 @@
 package recaf.paper.full;
 
+import java.util.function.Supplier;
+
 import recaf.paper.expr.IEval;
 
 public interface MuStmIEvalJavaAdapter<R,S> extends MuStmGenericJavaAdapter<R, S, IEval> {
 
 	@Override
-	default <T> T adapt(IEval e) {
-		try { return (T) e.eval(); } 
-		catch (Throwable t) {throw new RuntimeException(t);}
+	default <T> Supplier<T> adapt(IEval e) {
+		return () -> {
+			try { 
+				return (T) e.eval(); 
+			}
+			catch (Throwable t) {
+				throw new RuntimeException(t);
+			}
+		};
 	}
 
 }
