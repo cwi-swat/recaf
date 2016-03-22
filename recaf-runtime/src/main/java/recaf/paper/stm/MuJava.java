240f7752dd6ce74e34aa8dba3784e554b199feca
Aggelos Biboudis <biboudis@gmail.com>
2016-03-22 21:51:01 +0100

    Aligned with order of Figure 2

--- recaf-runtime/src/main/java/recaf/paper/stm/MuJava.java
+++ recaf-runtime/src/main/java/recaf/paper/stm/MuJava.java
@@ -7,11 +7,11 @@ public
 //BEGIN_MUJAVA_ALG
 interface MuJava<R, S> {
 	S Exp(Supplier<Void> e);
-	<T> S Decl(Supplier<T> e, Function<T, S> s);
-	<T> S For(Supplier<Iterable<T>> e, Function<T, S> s);
 	S If(Supplier<Boolean> c, S s1, S s2);
-	S Return(Supplier<R> e);
+	<T> S For(Supplier<Iterable<T>> e, Function<T, S> s);
+	<T> S Decl(Supplier<T> e, Function<T, S> s);
 	S Seq(S s1, S s2);
+	S Return(Supplier<R> e);
 	S Empty();
 }
 //END_MUJAVA_ALG
