59afa9ed0ae5adb895d9b0b86cdfe7da0d324853
Tijs van der Storm <storm@cwi.nl>
2016-03-22 18:23:01 +0100

    Markers

--- recaf-runtime/src/main/java/recaf/paper/demo/Tracing.java
+++ recaf-runtime/src/main/java/recaf/paper/demo/Tracing.java
@@ -4,14 +4,14 @@ import recaf.paper.expr.IEval;
 import recaf.paper.expr.MuExpJavaBase;
 
 public 
-//BEGIN_TRACING
 interface Tracing extends MuExpJavaBase {
+//BEGIN_TRACING
 	default IEval Var(String x, Object v){ 
 		return () -> {
 			System.err.println(x + " = " + v);
 			return MuExpJavaBase.super.Var(x, v).eval();
 		};
 	}
-}
 //END_TRACING
+}
 
